2d899a53f234507e4c6ae4b26e1b82d28159ba71
aya.salama@mentormate.com <ayoyasalama@gmail.com>
2023-03-15 18:30:11 +0200

    Testing

--- employes-poller/src/Test/saveQuestion.test.js
+++ employes-poller/src/Test/saveQuestion.test.js
@@ -1,9 +1,11 @@
-import { _saveQuestion } from "../utils/_DATA";
+// import { _saveQuestion } from "../utils/_DATA";
+import { saveQuestion } from "../utils/api";
+// import { _saveQuestionAnswer } from "../utils/_DATA";
 
 describe("saveQuestion", () => {
   it("will return success if a question in the desired foramat is passed", async () => {
     const testQuestion = { optionOneText:"option 1", optionTwoText :"option 2", author: "Aya Salama" };
-    const result = await _saveQuestion(testQuestion);
+    const result = await saveQuestion(testQuestion);
     expect(result.optionOne.text).toEqual(testQuestion.optionOneText);
     expect(result.optionTwo.text).toEqual(testQuestion.optionTwoText);
     expect(result.author).toEqual(testQuestion.author);
@@ -13,7 +15,7 @@ describe("saveQuestion", () => {
 describe("error saveQuestion", () => {
   it("will return Error if incorrect data is passed", async () => {
     const testQuestion = { optionOneText:"option 1"};
-    await expect(_saveQuestion(testQuestion)).rejects.toEqual(
+    await expect(saveQuestion(testQuestion)).rejects.toEqual(
       "Please provide optionOneText, optionTwoText, and author"
     );
   });
--- employes-poller/src/actions/Shared.js
+++ employes-poller/src/actions/Shared.js
@@ -1,18 +1,18 @@
 import { showLoading, hideLoading } from "react-redux-loading-bar";
 import { getIntialData } from "../utils/api";
-// import { setAuthedUser } from "./AuthedUser";
+import { setAuthedUser } from "./AuthedUser";
 import { getUsers } from "./Users";
 import { getPolls } from "./Polls";
 
 
 //TODO: remove used for testing purposes, use dynamic one
-// const SARAH_ID = "sarahedo";
+const SARAH_ID = "sarahedo";
 
 export function handleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
     return getIntialData().then(({users, polls}) => {
-      // dispatch(setAuthedUser(SARAH_ID));
+      dispatch(setAuthedUser(SARAH_ID));
       dispatch(getUsers(users));
       dispatch(getPolls(polls));
       dispatch(hideLoading);
