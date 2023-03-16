687bf72b779cad9933a77e13dcd9ddb442f5dd04
aya.salama@mentormate.com <ayoyasalama@gmail.com>
2023-03-16 17:11:20 +0200

    remove test code

--- employes-poller/src/actions/Shared.js
+++ employes-poller/src/actions/Shared.js
@@ -1,16 +1,12 @@
 import { showLoading, hideLoading } from "react-redux-loading-bar";
 import { getIntialData } from "../utils/api";
-import { setAuthedUser } from "./AuthedUser";
 import { getUsers } from "./Users";
 import { getPolls } from "./Polls";
-
-const zanobia = "zoshikanlu"
 
 export function handleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
     return getIntialData().then(({users, polls}) => {
-      dispatch(setAuthedUser(zanobia))
       dispatch(getUsers(users));
       dispatch(getPolls(polls));
       dispatch(hideLoading);
